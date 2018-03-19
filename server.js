dc18f222a4a3c91a3ed053f61351334373160606
nhrnjicKlika <nihad.hrnjic@klika.ba>
2018-03-19 09:47:24 +0100

    change port

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express')
 var app = express()
 app.use(express.static(__dirname + '/'))
 
+var port = process.env.port || 5000
+
 app.listen(5000, function(){
     console.log('listening at 5000')
 })
