fd4e8b928856f72225d18fa0861be7ae610e68ec
nhrnjicKlika <nihad.hrnjic@klika.ba>
2018-03-19 10:21:18 +0100

    change express port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 app.use(express.static(__dirname + '/'))
 
-var port = process.env.port || 5000
+var port = process.env.PORT || 5000
 
 console.log('hello from server.js')
 
