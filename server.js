675bacd6004dbc243dbc9dd7e9eeb3a14de9968b
nhrnjicKlika <nihad.hrnjic@klika.ba>
2018-03-19 09:43:21 +0100

    add server start callback

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ var express = require('express')
 var app = express()
 app.use(express.static(__dirname + '/'))
 
-app.listen(5000)
+app.listen(5000, function(){
+    console.log('listening at 5000')
+})
